effaa83ca2da7ab83ac1506dbf6114dabacc68d7
Michail Naiman <naiman.misha@gmail.com>
2020-11-19 04:26:38 +0300

    small changes

--- js.js
+++ js.js
@@ -1,7 +1,11 @@
 "use strict";
+const RECT_WIDTH = 60;
+const RECT_HEIGHT = 60;
+
 let canvas = document.querySelector("canvas");
 let ctx = canvas.getContext("2d");
-let deg; 
+
+let deg;
 let x = 450;
 let y = 270;
 let dx = 0;
@@ -18,79 +22,94 @@ let game_ON = true;
 
 
 
-let food_new = {
+let food = {
 	img: new Image(),
 	x: 300,
 	y: 300,
-	width: 60,
-	height: 60,
-	init: function() {
+	// width: RECT_WIDTH,
+	// height: RECT_HEIGHT,
+	init: function () {
 		this.img.src = "food.svg";
 
-		this.randomazer();
-		console.log(this.x);
-		console.log(this.y);
-		this.img.onload = function(){
-			ctx.drawImage(this.img,this.x,this.y,this.width,this.height);
+		// this.randomazer();
+		// console.log(this.x);
+		// console.log(this.y);
+		this.img.onload = function () {
+			ctx.drawImage(this.img, this.x, this.y, RECT_WIDTH, RECT_HEIGHT);
+			// ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
 		}.bind(this);
 	},
-	draw: function() {
-		this.clear();
-		this.randomazer();
-		ctx.drawImage(this.img,this.x,this.y,this.width,this.height);
+	draw: function () {
+		ctx.drawImage(this.img, this.x, this.y, RECT_WIDTH, RECT_HEIGHT);
+		// ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
 	},
-	clear: function() {
-		ctx.clearRect(this.x,this.y, this.width, this.height);
+	clear: function () {
+		ctx.clearRect(this.x, this.y, RECT_WIDTH, RECT_HEIGHT);
+		// ctx.clearRect(this.x, this.y, this.width, this.height);
 	},
-	randomazer: function (){
-		let randomY = Math.floor(Math.random()*(10));
-		let randomX = Math.floor(Math.random()*(15));
-		this.x = randomX*60;
-		this.y = randomY*60;
+	randomazer: function () {
+		let randomY = Math.floor(Math.random() * (10));
+		let randomX = Math.floor(Math.random() * (15));
+		this.x = randomX * 60;
+		this.y = randomY * 60;
 	}
 
 
 }
-let snake_new = {
+let snake = {
 	tail: new Image(),
 	body: new Image(),
 	body_p: new Image(),
 	head: new Image(),
 	size: 2,
-	width: 60,
-	height: 60,
-	x: [120,60,0],
-	y: [0,0,0],
-	init: function() {
+	// width: 60,
+	// height: 60,
+	x: [120, 60, 0],
+	y: [0, 0, 0],
+	angles: [90, 90, 90],
+	init: function () {
 		this.head.src = "head.svg";
 		this.body.src = "body.svg";
 		this.tail.src = "tail.svg";
 		this.body_p.src = "body_p.svg";
-		
-		this.head.onload = function(){
-			ctx.drawImage(this.head,120,0,this.width,this.height);
-		}.bind(this);
-		this.body.onload = function(){
-			ctx.drawImage(this.body,60,0,this.width,this.height);
-		}.bind(this);
-		this.tail.onload = function(){
-			ctx.drawImage(this.tail,0,0,this.width,this.height);
-		}.bind(this);
-		this.body_p.onload = function(){
-			ctx.drawImage(this.body_p,0,60,this.width,this.height);
-		}.bind(this);
-	},
-	draw: function(x, y, image) {
-		ctx.drawImage(image,x,y,this.width,this.height);
-	},
-	clearHead: function() {
-		ctx.clearRect(this.x[0],this.y[0], this.width, this.height);
-	},
-	clearTail: function() {
-		ctx.clearRect(this.x[this.size - 1],y[this.size - 1], this.width, this.height);
-	},
-	new_coordinate(x, y) {
 
+		this.head.onload = function () {
+			// ctx.drawImage(this.head, 120, 0, rect, this.height);
+			// ctx.drawImage(this.head, 120, 0, this.width, this.height);
+		}.bind(this);
+		this.body.onload = function () {
+			// ctx.drawImage(this.body, 60, 0, this.width, this.height);
+		}.bind(this);
+		this.tail.onload = function () {
+			// ctx.drawImage(this.tail, 0, 0, this.width, this.height);
+		}.bind(this);
+		this.body_p.onload = function () {
+			// ctx.drawImage(this.body_p, 0, 60, this.width, this.height);
+		}.bind(this);
+	},
+	draw: function (x, y, image, degree) {
+		let angle = (degree - 90) * Math.PI / 180;
+		ctx.save();
+		ctx.translate(x, y);
+		ctx.translate(RECT_WIDTH / 2, RECT_HEIGHT / 2);
+		// ctx.translate(this.width / 2, this.height / 2);
+		ctx.rotate(angle);
+		ctx.drawImage(image, -(RECT_WIDTH / 2), -(RECT_HEIGHT / 2), RECT_WIDTH, RECT_HEIGHT);
+		// ctx.drawImage(image, -(this.width / 2), -(this.height / 2), this.width, this.height);
+		ctx.restore();
+	},
+	/*clearHead: function () {
+		ctx.clearRect(this.x[0], this.y[0], this.width, this.height);
+	},
+	clearTail: function () {
+		ctx.clearRect(this.x[this.size - 1], y[this.size - 1], this.width, this.height);
+	},*/
+	new_coordinate(dx, dy) {
+
+	},
+	clear: function (x, y) {
+		ctx.clearRect(x, y, RECT_WIDTH, RECT_HEIGHT);
+		// ctx.clearRect(x, y, this.width, this.height);
 	}
 };
 
@@ -101,20 +120,24 @@ let snake_new = {
 
 // randomazer();
 // food();
-food_new.init();
-snake_new.init();
-console.log(snake_new);
-snake_new.draw();
+food.init();
+snake.init();
+setTimeout(function () {
+	snake.draw(120, 120, snake.head, 180);
+}, 1000);
+// snake.draw(120, 120, snake.head, 180);
+// console.log(snake);
+// snake_new.draw();
 // setInterval(function(){
 // 	food_new.draw();
 // }, 2000);
 // new_draw();
 // draw();
-	// interval = setInterval(draw,time);
+// interval = setInterval(draw,time);
 
 
 
-document.addEventListener("keydown", function(event) { 
+document.addEventListener("keydown", function (event) {
 	if (event.repeat) {
 		return;
 	}
